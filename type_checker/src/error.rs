04f47f9037104634c3cb2ac7bfb25ac7a7a4e7af
APIBox <LinkerVB@outlook.com>
2026-01-01 10:09:13 +0800

    feat(type_checker): 为 TypeCheckerErrorKind 实现 Display

--- type_checker/src/error.rs
+++ type_checker/src/error.rs
@@ -1,4 +1,4 @@
-use std::sync::Arc;
+use std::{fmt::Display, sync::Arc};
 
 use token::token::Token;
 
@@ -8,6 +8,19 @@ pub enum TypeCheckerErrorKind {
     TypeNotFound,
     TypeMismatch,
     Other,
+}
+
+impl Display for TypeCheckerErrorKind {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = match self {
+            TypeCheckerErrorKind::VariableNotFound => "variable not found",
+            TypeCheckerErrorKind::TypeNotFound => "type not found",
+            TypeCheckerErrorKind::TypeMismatch => "type mismatch",
+            TypeCheckerErrorKind::Other => "other",
+        };
+
+        write!(f, "{s}")
+    }
 }
 
 #[derive(Clone, Debug, PartialEq, Eq)]
