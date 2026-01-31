0711ca4db3cfadb92ec26f3096983cccdf5f6c68
APIBox <LinkerVB@outlook.com>
2026-01-31 16:20:14 +0800

    feat(parser): 为 Struct 添加解析泛型支持

--- parser/src/parse_functions/parse_struct.rs
+++ parser/src/parse_functions/parse_struct.rs
@@ -4,6 +4,8 @@ use token::token_type::TokenType;
 use crate::{ParseResult, Parser, parse_functions::{parse_ident::parse_ident, parse_type_hint::parse_type_hint}};
 
 pub fn parse_struct(parser: &mut Parser) -> ParseResult<Statement> {
+    let mut generics = vec![];
+
     let token = parser.cur_token.clone();
 
     parser.expect_peek(TokenType::Ident)?;
@@ -14,6 +16,12 @@ pub fn parse_struct(parser: &mut Parser) -> ParseResult<Statement> {
         value: parser.cur_token.value.clone(),
         token: parser.cur_token.clone(),
     };
+
+    if parser.peek_token_is(TokenType::Lt) {
+        parser.next_token(); // 前进到 <
+
+        generics = parser.parse_expression_list(TokenType::Gt)?;
+    }
 
     parser.expect_peek(TokenType::LBrace)?;
 
@@ -29,5 +37,5 @@ pub fn parse_struct(parser: &mut Parser) -> ParseResult<Statement> {
     // 移除 TypeHint 解析函数
     parser.prefix_parse_fn_map.insert(TokenType::Ident, parse_ident);
 
-    Ok(Statement::Struct { token, name, fields })
+    Ok(Statement::Struct { token, name, fields, generics })
 }
