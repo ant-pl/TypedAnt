ff18a6bffcefdd73a7fcdca7b2a3d042d80bd67e
APIBox <LinkerVB@outlook.com>
2026-02-04 15:51:14 +0800

    feat(type_checker): 添加 类型上下文 (TypeContext)

---
+++ type_checker/src/ty_context.rs
@@ -0,0 +1,51 @@
+use std::sync::{Arc, Mutex};
+
+use crate::{
+    table::TypeTable,
+    ty::{Ty, TyId},
+};
+
+#[derive(Debug, Clone)]
+pub struct TypeContext {
+    pub types: Vec<Ty>,
+    pub table: Arc<Mutex<TypeTable>>,
+}
+
+impl TypeContext {
+    pub fn alloc(&mut self, ty: Ty) -> TyId {
+        for (i, old) in self.types.iter().enumerate() {
+            if *old == ty {
+                return i;
+            }
+        }
+
+        let id = self.types.len();
+        self.types.push(ty);
+        id
+    }
+}
+
+impl TypeContext {
+    pub fn get(&self, id: TyId) -> &Ty {
+        &self.types[id]
+    }
+
+    pub fn get_mut(&mut self, id: TyId) -> &mut Ty {
+        &mut self.types[id]
+    }
+}
+
+impl TypeContext {
+    pub fn new() -> Self {
+        let mut me = Self {
+            types: vec![],
+            table: Arc::new(Mutex::new(TypeTable::new())),
+        };
+
+        let new_table = TypeTable::new().init(&mut me);
+
+        me.table = Arc::new(Mutex::new(new_table));
+
+        me
+    }
+}
