79c06f6ed15c9758528003110f9b9986a3a007e9
APIBox <LinkerVB@outlook.com>
2026-02-28 16:54:55 +0800

    feat(type_infer): 支持泛型函数

--- type_checker/src/type_infer/mod.rs
+++ type_checker/src/type_infer/mod.rs
@@ -1,29 +1,287 @@
 pub mod constraint;
 pub mod infer_context;
+
+use std::collections::HashMap;
+
+use ast::node::GetToken;
+use ast::{ExprId, StmtId};
 use token::token::Token;
 
 use crate::CheckResult;
 use crate::error::{TypeCheckerError, TypeCheckerErrorKind};
+use crate::module::TypedModule;
 use crate::ty::{Ty, TyId};
 use crate::ty_context::TypeContext;
 use crate::type_infer::constraint::Constraint;
 use crate::type_infer::infer_context::InferContext;
+use crate::typed_ast::typed_expr::TypedExpression;
+use crate::typed_ast::typed_stmt::TypedStatement;
+use crate::typed_ast::{GetType, SetType};
 
-pub struct TypeInfer<'a> {
-    infer_ctx: &'a mut InferContext<'a>,
+pub struct TypeInfer<'a, 'b, 'c> {
+    pub infer_ctx: &'a mut InferContext<'b, 'c>,
 }
 
-impl<'a> TypeInfer<'a> {
-    pub fn new(infer_ctx: &'a mut InferContext<'a>) -> Self {
+impl<'c, 'b, 'a> TypeInfer<'a, 'b, 'c> {
+    pub fn new(infer_ctx: &'a mut InferContext<'b, 'c>) -> Self {
         Self { infer_ctx }
     }
 
+    #[inline(always)]
     fn tcx(&mut self) -> &mut TypeContext {
-        self.infer_ctx.tcx
+        self.infer_ctx.module.tcx_mut()
     }
 
+    #[inline(always)]
     fn tcx_ref(&self) -> &TypeContext {
-        self.infer_ctx.tcx
+        self.infer_ctx.module.tcx_ref()
+    }
+
+    /// 除非你打算长期持有 Module 否则不推荐使用这个函数
+    /// 使用伴随整个 TypeInfer 的生命周期 'a 总是有代价的
+    #[inline(always)]
+    fn module(&'c mut self) -> &'a mut TypedModule<'b> {
+        self.infer_ctx.module
+    }
+
+    #[inline(always)]
+    fn module_ref(&self) -> &TypedModule<'_> {
+        self.infer_ctx.module
+    }
+
+    pub fn infer(&mut self) -> CheckResult<()> {
+        let stmt_len = self.module_ref().typed_stmts.len();
+
+        for i in 0..stmt_len {
+            self.infer_stmt(StmtId(i))?;
+        }
+
+        self.finalize();
+
+        Ok(())
+    }
+
+    fn infer_stmt(&mut self, stmt_id: StmtId) -> CheckResult<()> {
+        let stmt = self.module_ref().typed_stmts[stmt_id.0].clone();
+
+        let ty = match stmt {
+            TypedStatement::ExpressionStatement(_, id, _) => {
+                Some(self.infer_expr(id)?)
+            }
+
+            TypedStatement::Let { value: id, ty, .. } => {
+                let expr = self.module_ref().get_expr(id).unwrap().clone();
+
+                self.unify(expr.get_type(), ty, expr.token())?;
+
+                Some(self.infer_expr(id)?)
+            }
+
+            TypedStatement::Return { expr: id, .. } => {
+                Some(self.infer_expr(id)?)
+            }
+
+            _ => None,
+        };
+
+        if let Some(it) = ty {
+            self.infer_ctx.module.typed_stmts[stmt_id.0].set_type(it);
+        }
+
+        Ok(())
+    }
+
+    fn infer_expr(&mut self, expr_id: ExprId) -> CheckResult<TyId> {
+        let expr = self.module_ref().get_expr(expr_id).cloned().unwrap();
+
+        let ty = match expr {
+            TypedExpression::Int { ty, .. } => ty,
+            TypedExpression::StrLiteral { ty, .. } => ty,
+            TypedExpression::Bool { ty, .. } => ty,
+            TypedExpression::BigInt { ty, .. } => ty,
+            TypedExpression::TypeHint(_, _, ty) => ty,
+
+            TypedExpression::If {
+                consequence,
+                else_block,
+                ..
+            } => {
+                let then_block_ty = self.infer_expr(consequence)?;
+
+                if let Some(it) = else_block.and_then(|it| Some(self.infer_expr(it))) {
+                    return it;
+                }
+
+                then_block_ty
+            }
+
+            TypedExpression::Infix {
+                left: left_id,
+                right: right_id,
+                ty,
+                ..
+            } => {
+                let left = self.module_ref().get_expr(left_id).unwrap().clone();
+                let right = self.module_ref().get_expr(right_id).unwrap().clone();
+
+                self.unify(left.get_type(), right.get_type(), right.token())?;
+
+                self.infer_expr(left_id)?;
+                self.infer_expr(right_id)?;
+
+                ty
+            }
+
+            TypedExpression::SizeOf(_, val, ty) => {
+                self.infer_expr(val)?;
+
+                ty
+            }
+
+            TypedExpression::BuildStruct(_, _, fields, ty) => {
+                for (_, val) in fields {
+                    self.infer_expr(val)?;
+                }
+
+                ty
+            }
+
+            TypedExpression::FieldAccess(_, obj, _, ty) => {
+                self.infer_expr(obj)?;
+
+                ty
+            }
+
+            TypedExpression::Block(_, stmts, ty) => {
+                for stmt in stmts {
+                    self.infer_stmt(stmt)?;
+                }
+
+                ty
+            }
+
+            TypedExpression::Assign {
+                left: left_id,
+                right: right_id,
+                ty,
+                ..
+            } => {
+                let left = self.module_ref().get_expr(left_id).unwrap().clone();
+                let right = self.module_ref().get_expr(right_id).unwrap().clone();
+
+                self.unify(left.get_type(), right.get_type(), right.token())?;
+
+                self.infer_expr(left_id)?;
+                self.infer_expr(right_id)?;
+
+                ty
+            }
+
+            TypedExpression::BoolAnd {
+                left: left_id,
+                right: right_id,
+                ty,
+                ..
+            } => {
+                let left = self.module_ref().get_expr(left_id).unwrap().clone();
+                let right = self.module_ref().get_expr(right_id).unwrap().clone();
+
+                let bool_ty = self.tcx().alloc(Ty::Bool);
+
+                self.unify(left.get_type(), bool_ty, left.token())?;
+                self.unify(right.get_type(), bool_ty, right.token())?;
+
+                self.infer_expr(left_id)?;
+                self.infer_expr(right_id)?;
+
+                ty
+            }
+
+            TypedExpression::BoolOr {
+                left: left_id,
+                right: right_id,
+                ty,
+                ..
+            } => {
+                let left = self.module_ref().get_expr(left_id).unwrap().clone();
+                let right = self.module_ref().get_expr(right_id).unwrap().clone();
+
+                let bool_ty = self.tcx().alloc(Ty::Bool);
+
+                self.unify(left.get_type(), bool_ty, left.token())?;
+                self.unify(right.get_type(), bool_ty, right.token())?;
+
+                self.infer_expr(left_id)?;
+                self.infer_expr(right_id)?;
+
+                ty
+            }
+
+            TypedExpression::Function { block, ty, .. } => {
+                self.infer_expr(block)?;
+
+                ty
+            }
+
+            TypedExpression::Call { func, args, .. } => {
+                let callee_ty =
+                    self.infer_expr(func)?;
+
+                let arg_types = args.iter().map(|it| self.get_expr_tyid(*it)).collect();
+
+                let new_ret_ty = self.infer_ctx.alloc_infer_ty();
+
+                let new_func_ty = self.tcx().alloc(Ty::Function {
+                    params_type: arg_types,
+                    ret_type: new_ret_ty,
+                    is_variadic: false,
+                });
+
+                self.unify(
+                    callee_ty,
+                    new_func_ty,
+                    self.module_ref().get_expr(func).unwrap().token(),
+                )?;
+
+                new_ret_ty
+            }
+
+            TypedExpression::Ident(_, ty) => {
+                if let Ty::Function {
+                    params_type,
+                    ret_type,
+                    ..
+                } = self.tcx_ref().get(ty)
+                {
+                    let mut generic_params = params_type
+                        .iter()
+                        .map(|it| self.tcx_ref().get(*it))
+                        .filter(|it| matches!(it, Ty::Generic(..)))
+                        .cloned()
+                        .map(|it| {
+                            if let Ty::Generic(name, _impl_traits) = it {
+                                name.to_string()
+                            } else {
+                                unreachable!()
+                            }
+                        })
+                        .collect::<Vec<_>>();
+
+                    if let Ty::Generic(name, _impl_traits) = self.tcx_ref().get(*ret_type) {
+                        generic_params.push(name.to_string());
+                    }
+
+                    let new_ty = self.instantiate(ty, generic_params.as_slice());
+                    new_ty
+                } else {
+                    ty
+                }
+            }
+        };
+
+        self.infer_ctx.module.typed_exprs[expr_id.0].set_type(ty);
+
+        return Ok(ty);
     }
 
     pub fn unify_all(&mut self, constraints: Vec<Constraint>) -> CheckResult<()> {
@@ -136,36 +394,81 @@ impl<'a> TypeInfer<'a> {
         // 如果 real_id 指向的依然是 Ty::Infer，说明这个变量到最后也没推导出来（报错点）
         real_id
     }
+
+    fn get_expr_tyid(&self, exprid: ExprId) -> TyId {
+        self.infer_ctx.module.get_expr(exprid).unwrap().get_type()
+    }
+
+    fn get_stmt_tyid(&self, stmtid: StmtId) -> TyId {
+        self.infer_ctx.module.get_stmt(stmtid).unwrap().get_type()
+    }
+
+    /// 替换泛型到推导类型
+    fn substitute(&mut self, ty_id: TyId, mapping: &HashMap<String, TyId>) -> TyId {
+        let ty = self.tcx_ref().get(ty_id).clone();
+
+        match ty {
+            Ty::Generic(name, _) => mapping.get(name.as_ref()).copied().unwrap_or(ty_id),
+            Ty::Function {
+                params_type,
+                ret_type,
+                is_variadic,
+            } => {
+                let new_params = params_type
+                    .iter()
+                    .map(|p| self.substitute(*p, mapping))
+                    .collect();
+                let new_ret = self.substitute(ret_type, mapping);
+
+                // 重新打包成一个新的函数 TyId 返回
+                self.tcx().alloc(Ty::Function {
+                    params_type: new_params,
+                    ret_type: new_ret,
+                    is_variadic,
+                })
+            }
+
+            _ => ty_id,
+        }
+    }
+
+    fn instantiate(&mut self, ty_id: TyId, generic_params: &[String]) -> TyId {
+        if generic_params.is_empty() {
+            return ty_id;
+        }
+
+        let ty = self.tcx_ref().get(ty_id).clone();
+
+        let mut mapping = HashMap::new();
+
+        for param in generic_params {
+            mapping.insert(param.clone(), self.infer_ctx.alloc_infer_ty());
+        }
+
+        match ty {
+            _ => self.substitute(ty_id, &mapping),
+        }
+    }
 }
 
-impl<'a> TypeInfer<'a> {
+impl<'c, 'b, 'a> TypeInfer<'a, 'b, 'c> {
     /// 将最终结果注入 TypeContext，彻底抹除占位符
     pub fn finalize(&mut self) {
-        // 拿一波新类型和原类型表
-        let subs = &self.infer_ctx.substitutions;
-        let tcx = &mut self.infer_ctx.tcx;
+        // 替换推导类型
+        for expr_idx in 0..self.infer_ctx.module.typed_exprs.len() {
+            let ty = self.infer_ctx.module.typed_exprs[expr_idx].get_type();
+            let real_ty = self.follow(ty);
 
-        for i in 0..tcx.types.len() {
-            let mut current_id = i;
-            
-            // 追踪这个坑位最终指向谁
-            // 这里复用 follow 逻辑 (为这里单独写个不依赖 self 的函数不值得也没必要)
-            while let Ty::Infer(infer_id) = &tcx.types[current_id] {
-                if let Some(target_id) = subs.get(infer_id) {
-                    current_id = *target_id;
-                } else {
-                    break;
-                }
-            }
-
-            // 3. 如果发现最终指向的不是自己，说明这是一个推导出来的变量
-            if current_id != i {
-                // 强制暴力修改
-                tcx.types[i] = tcx.types[current_id].clone();
-            }
+            let expr = &mut self.infer_ctx.module.typed_exprs[expr_idx];
+            expr.set_type(real_ty);
         }
-        
-        // 执行到这里，tcx.types 里大部分的 Ty::Infer 余孽只要九族有记录，
-        // 就全都被杀完了 (成了具体的类型 (如 i64, bool 等))
+
+        for stmt_idx in 0..self.infer_ctx.module.typed_stmts.len() {
+            let ty = self.infer_ctx.module.typed_stmts[stmt_idx].get_type();
+            let real_ty = self.follow(ty);
+
+            let stmt = &mut self.infer_ctx.module.typed_stmts[stmt_idx];
+            stmt.set_type(real_ty);
+        }
     }
 }
