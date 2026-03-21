e4b119dbf8fd0a1e30b4fc33ae5cff621f8bed30
APIBox <LinkerVB@outlook.com>
2026-03-21 10:48:30 +0800

    feat(infer_context): 支持分配整数推导类型

--- type_checker/src/type_infer/infer_context.rs
+++ type_checker/src/type_infer/infer_context.rs
@@ -20,6 +20,15 @@ impl InferContext<'_, '_> {
         // 将其包装成 Ty::Infer 存入类型列表并返回它的 TyId
         self.module.tcx_mut().alloc(Ty::Infer(id))
     }
+
+    /// 分配欲推导整数类型 (与普通 Infer 类型共用替换)
+    pub fn alloc_infer_int(&mut self) -> TyId {
+        let id = self.next_infer_id;
+        self.next_infer_id += 1; // 每次使用后加 1，确保下次不重复
+        
+        // 将其包装成 Ty::InferInt 存入类型列表并返回它的 TyId
+        self.module.tcx_mut().alloc(Ty::InferInt(id))
+    }
 }
 
 impl<'b, 'a> InferContext<'a, 'b> {
