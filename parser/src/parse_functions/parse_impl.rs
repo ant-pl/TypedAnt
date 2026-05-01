c2e0608586afaf5cd0958f6ae6f7cc4ca9d110d6
APIBox <LinkerVB@outlook.com>
2026-05-01 19:10:13 +0800

    feat: 支持解析 impl 块的泛型

--- parser/src/parse_functions/parse_impl.rs
+++ parser/src/parse_functions/parse_impl.rs
@@ -4,7 +4,16 @@ use token::token_type::TokenType;
 use crate::{ParseResult, Parser, parse_functions::parse_block::parse_block_stmt};
 
 pub fn parse_impl(parser: &mut Parser) -> ParseResult<Statement> {
+    let mut generics = vec![];
+    let mut type_paths = vec![];
+
     let token = parser.cur_token.clone();
+
+    if parser.peek_token_is(TokenType::Lt) {
+        parser.next_token(); // 前进到 <
+
+        generics = parser.parse_expression_list(TokenType::Gt)?;
+    }
 
     parser.expect_peek(TokenType::Ident)?;
 
@@ -14,6 +23,12 @@ pub fn parse_impl(parser: &mut Parser) -> ParseResult<Statement> {
         value: parser.cur_token.value.clone(),
         token: parser.cur_token.clone(),
     };
+
+    if parser.peek_token_is(TokenType::Lt) {
+        parser.next_token(); // 前进到 <
+
+        type_paths = parser.parse_expression_list(TokenType::Gt)?;
+    }
 
     let mut for_ = None;
 
@@ -35,6 +50,8 @@ pub fn parse_impl(parser: &mut Parser) -> ParseResult<Statement> {
         token,
         impl_,
         for_,
+        generics,
+        type_paths,
         block: Box::new(parse_block_stmt(parser)?),
     })
 }
