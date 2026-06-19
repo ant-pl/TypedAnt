d1cea7cb0f983ac2d2ee2b36c789196eff3a5c31
APIBox <LinkerVB@outlook.com>
2026-06-19 14:39:15 +0800

    fix: 修复了在 unexpect_token_err 函数中构造错误对象时不使用错误处 token 的 bug

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -457,12 +457,12 @@ impl Parser {
 
     pub fn unexpect_token_err(&self, expected: TokenType, got: Token) -> ParseResult<()> {
         Err(ParserError {
-            token: got,
+            token: got.clone(),
             kind: ParserErrorKind::NotExpectedTokenType,
             message: Some(
                 format!(
                     "expected token: {:#?}, got: {:#?}",
-                    expected, self.peek_token.token_type
+                    expected, got
                 )
                 .into(),
             ),
